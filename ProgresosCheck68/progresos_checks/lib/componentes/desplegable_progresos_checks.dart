fe1c33dfdca465e85ee00b54a1bbed378f9e1860
DanielLobato <daniel.lobato.asensio@gregoriofer.com>
2025-05-08 13:56:12 +0200

    Scroll

--- ProgresosCheck68/progresos_checks/lib/componentes/desplegable_progresos_checks.dart
+++ ProgresosCheck68/progresos_checks/lib/componentes/desplegable_progresos_checks.dart
@@ -18,7 +18,7 @@ class DesplegableProgresosChecks extends StatefulWidget {
 class _DesplegableProgresosChecksState
     extends State<DesplegableProgresosChecks> {
   bool marcarTodos = false;
-  List<bool> values = [false, false, false, false, false];
+  List<bool> values = [false, false, false, false, false, false, false, false, false, false, false, false, false];
   final List<Check> checks = [
       Check(
     postId: 1,
@@ -95,6 +95,126 @@ class _DesplegableProgresosChecksState
     checkThigh: "70",
     checkCalf: "60",
   ),
+    Check(
+    postId: 2,
+    postTitle: "Check 2",
+    createdDate: "2021-02-01",
+    modifiedDate: "2021-02-01",
+    checkWeight: "85",
+    checkNeck: "45",
+    checkChest: "105",
+    checkBiceps: "40",
+    checkForearm: "30",
+    checkWaist: "90",
+    checkHip: "100",
+    checkThigh: "55",
+    checkCalf: "45",
+  ),
+  Check(
+    postId: 3,
+    postTitle: "Check 3",
+    createdDate: "2021-03-01",
+    modifiedDate: "2021-03-01",
+    checkWeight: "80",
+    checkNeck: "50",
+    checkChest: "110",
+    checkBiceps: "45",
+    checkForearm: "35",
+    checkWaist: "95",
+    checkHip: "105",
+    checkThigh: "60",
+    checkCalf: "50",
+  ),
+  Check(
+    postId: 4,
+    postTitle: "Check 4",
+    createdDate: "2021-04-01",
+    modifiedDate: "2021-04-01",
+    checkWeight: "75",
+    checkNeck: "55",
+    checkChest: "115",
+    checkBiceps: "50",
+    checkForearm: "40",
+    checkWaist: "100",
+    checkHip: "110",
+    checkThigh: "65",
+    checkCalf: "55",
+  ),
+  Check(
+    postId: 5,
+    postTitle: "Check 5",
+    createdDate: "2021-05-01",
+    modifiedDate: "2021-05-01",
+    checkWeight: "70",
+    checkNeck: "60",
+    checkChest: "120",
+    checkBiceps: "55",
+    checkForearm: "45",
+    checkWaist: "105",
+    checkHip: "115",
+    checkThigh: "70",
+    checkCalf: "60",
+  ),
+    Check(
+    postId: 2,
+    postTitle: "Check 2",
+    createdDate: "2021-02-01",
+    modifiedDate: "2021-02-01",
+    checkWeight: "85",
+    checkNeck: "45",
+    checkChest: "105",
+    checkBiceps: "40",
+    checkForearm: "30",
+    checkWaist: "90",
+    checkHip: "100",
+    checkThigh: "55",
+    checkCalf: "45",
+  ),
+  Check(
+    postId: 3,
+    postTitle: "Check 3",
+    createdDate: "2021-03-01",
+    modifiedDate: "2021-03-01",
+    checkWeight: "80",
+    checkNeck: "50",
+    checkChest: "110",
+    checkBiceps: "45",
+    checkForearm: "35",
+    checkWaist: "95",
+    checkHip: "105",
+    checkThigh: "60",
+    checkCalf: "50",
+  ),
+  Check(
+    postId: 4,
+    postTitle: "Check 4",
+    createdDate: "2021-04-01",
+    modifiedDate: "2021-04-01",
+    checkWeight: "75",
+    checkNeck: "55",
+    checkChest: "115",
+    checkBiceps: "50",
+    checkForearm: "40",
+    checkWaist: "100",
+    checkHip: "110",
+    checkThigh: "65",
+    checkCalf: "55",
+  ),
+  Check(
+    postId: 5,
+    postTitle: "Check 5",
+    createdDate: "2021-05-01",
+    modifiedDate: "2021-05-01",
+    checkWeight: "70",
+    checkNeck: "60",
+    checkChest: "120",
+    checkBiceps: "55",
+    checkForearm: "45",
+    checkWaist: "105",
+    checkHip: "115",
+    checkThigh: "70",
+    checkCalf: "60",
+  ),
   
   
   
@@ -106,75 +226,59 @@ class _DesplegableProgresosChecksState
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Container(
-              decoration: BoxDecoration(
-                color: AppColores.fondoComponentes,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      width: double.infinity,
-                      child: Text("Checks".toUpperCase(), style: EstilosTexto.titulos, textAlign: TextAlign.center,)),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text("Marcar todos", style: EstilosTexto.checks),
-                          ),
-                        ),
-                        Checkbox(
-                          value: marcarTodos,
-                          onChanged: (bool? newValue) {
-                            setState(() {
-                              marcarTodos = newValue!;
-                              values = List.generate(
-                                values.length,
-                                (index) => marcarTodos,
-                              );
-                              widget.onSelectionChanged(getSelectedChecks());
-                            });
-                          },
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: List.generate(checks.length, (index) {
-                        return Row(
-                          children: [
-                            Expanded(child: desplegable(checks[index])),
-                            Checkbox(
-                              value: values[index],
-                              onChanged: (bool? newValue) {
-                                setState(() {
-                                  values[index] = newValue!;
-                                  marcarTodos = values.every((val) => val);
-                                  widget.onSelectionChanged(
-                                    getSelectedChecks(),
-                                  );
-                                });
-                              },
-                            ),
-                          ],
-                        );
-                      }),
-                    ),
-                  ],
+    return SingleChildScrollView(
+      
+      child: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            child: Text("Checks".toUpperCase(), style: EstilosTexto.titulos, textAlign: TextAlign.center,)),
+          Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text("Marcar todos", style: EstilosTexto.checks),
                 ),
               ),
-            ),
+              Checkbox(
+                value: marcarTodos,
+                onChanged: (bool? newValue) {
+                  setState(() {
+                    marcarTodos = newValue!;
+                    values = List.generate(
+                      values.length,
+                      (index) => marcarTodos,
+                    );
+                    widget.onSelectionChanged(getSelectedChecks());
+                  });
+                },
+              ),
+            ],
           ),
-        ),
-      ],
+          Column(
+            children: List.generate(checks.length, (index) {
+              return Row(
+                children: [
+                  Expanded(child: desplegable(checks[index])),
+                  Checkbox(
+                    value: values[index],
+                    onChanged: (bool? newValue) {
+                      setState(() {
+                        values[index] = newValue!;
+                        marcarTodos = values.every((val) => val);
+                        widget.onSelectionChanged(
+                          getSelectedChecks(),
+                        );
+                      });
+                    },
+                  ),
+                ],
+              );
+            }),
+          ),
+        ],
+      ),
     );
   }
 
--- ProgresosCheck68/progresos_checks/lib/componentes/grafico_comparacion.dart
+++ ProgresosCheck68/progresos_checks/lib/componentes/grafico_comparacion.dart
@@ -17,162 +17,97 @@ class _GraficoComparacionState extends State<GraficoComparacion> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Container(
-              decoration: BoxDecoration(
-                color: AppColores.fondoComponentes,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      width: double.infinity,
-                      child: Text(
-                        "Gráfico de comparación".toUpperCase(),
-                        style: EstilosTexto.titulos,
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    Expanded(child: buildChart()),
-                  ],
-                ),
-              ),
-            ),
+        Container(
+          width: double.infinity,
+          child: Text(
+            "Gráfico de comparación".toUpperCase(),
+            style: EstilosTexto.titulos,
+            textAlign: TextAlign.center,
           ),
         ),
+        const SizedBox(height: 20),
+        Expanded(child: buildChart()),
       ],
     );
   }
 
   // Método para construir el gráfico
   Widget buildChart() {
-    final selectedChecks = widget.checksSeleccionados;
+  final selectedChecks = widget.checksSeleccionados;
 
-    if (selectedChecks.isEmpty) {
-      return Center(child: Text("No hay datos seleccionados"));
-    }
+  if (selectedChecks.isEmpty) {
+    return Center(child: Text("No hay datos seleccionados"));
+  }
 
-    return BarChart(
-      BarChartData(
-        maxY: 200,
-        barGroups:
-            selectedChecks.map((check) {
+  // Calcular un ancho suficiente basado en la cantidad de checks
+  double chartWidth = selectedChecks.length * 200;
 
-              
-              return BarChartGroupData(
-                x: check.postId,
-                barRods: [
-                  BarChartRodData(
-                    toY: double.parse(check.checkWeight),
-                    color: Colors.brown,
-                    width: 20,
-                    
-                  ),
-                  BarChartRodData(
-                    toY: double.parse(check.checkNeck),
-                    color: Colors.red,
-                    width: 20,
-                  ),
-                  BarChartRodData(
-                    toY: double.parse(check.checkChest),
-                    color: Colors.orange,
-                    width: 20,
-                  ),
-                  BarChartRodData(
-                    toY: double.parse(check.checkBiceps),
-                    color: Colors.yellow,
-                    width: 20,
-                  ),
-                  BarChartRodData(
-                    toY: double.parse(check.checkForearm),
-                    color: Colors.green,
-                    width: 20,
-                  ),
-                  BarChartRodData(
-                    toY: double.parse(check.checkWaist),
-                    color: Colors.lightBlue,
-                    width: 20,
-                  ),
-                  BarChartRodData(
-                    toY: double.parse(check.checkHip),
-                    color: Colors.blueAccent,
-                    width: 20,
-                  ),
-                  BarChartRodData(
-                    toY: double.parse(check.checkThigh),
-                    color: Colors.purple,
-                    width: 20,
-                  ),
-                  BarChartRodData(
-                    toY: double.parse(check.checkCalf),
-                    color: Colors.pink,
-                    width: 20,
-                  ),
-                ],
-              );
-            }).toList(),
-
-       
-
-        borderData: FlBorderData(show: false),
-        titlesData: FlTitlesData(
-          leftTitles: AxisTitles(
-            sideTitles: SideTitles(
-              reservedSize: 40,
-              showTitles: true,
-              interval: 10,
-              getTitlesWidget: (value, meta) {
-                return Transform.rotate(
-                  angle: 0, // Esto los mantiene horizontales
-                  child: Text(
+  return SingleChildScrollView(
+    scrollDirection: Axis.horizontal,
+    child: SizedBox(
+      width: chartWidth,
+      height: 400, // Puedes ajustar la altura según tu diseño
+      child: BarChart(
+        BarChartData(
+          maxY: 200,
+          barGroups: selectedChecks.map((check) {
+            return BarChartGroupData(
+              x: check.postId,
+              barRods: [
+                BarChartRodData(toY: double.parse(check.checkWeight), color: Colors.brown, width: 20),
+                BarChartRodData(toY: double.parse(check.checkNeck), color: Colors.red, width: 20),
+                BarChartRodData(toY: double.parse(check.checkChest), color: Colors.orange, width: 20),
+                BarChartRodData(toY: double.parse(check.checkBiceps), color: Colors.yellow, width: 20),
+                BarChartRodData(toY: double.parse(check.checkForearm), color: Colors.green, width: 20),
+                BarChartRodData(toY: double.parse(check.checkWaist), color: Colors.lightBlue, width: 20),
+                BarChartRodData(toY: double.parse(check.checkHip), color: Colors.blueAccent, width: 20),
+                BarChartRodData(toY: double.parse(check.checkThigh), color: Colors.purple, width: 20),
+                BarChartRodData(toY: double.parse(check.checkCalf), color: Colors.pink, width: 20),
+              ],
+            );
+          }).toList(),
+          borderData: FlBorderData(show: false),
+          titlesData: FlTitlesData(
+            leftTitles: AxisTitles(
+              sideTitles: SideTitles(
+                reservedSize: 40,
+                showTitles: true,
+                interval: 10,
+                getTitlesWidget: (value, meta) {
+                  return Text(
                     value.toInt().toString(),
                     style: TextStyle(fontSize: 20),
-                  ),
-                );
-              },
+                  );
+                },
+              ),
             ),
-          ),
-          rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
-          topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
-          bottomTitles: AxisTitles(
-            sideTitles: SideTitles(
-              showTitles: true,
-              reservedSize: 70,
-              getTitlesWidget: (double value, TitleMeta meta) {
-                // Filtrar correctamente el `Check` por su `postId`
+            rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+            topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+            bottomTitles: AxisTitles(
+              sideTitles: SideTitles(
+                showTitles: true,
+                reservedSize: 70,
+                getTitlesWidget: (double value, TitleMeta meta) {
+                   // Filtrar correctamente el Check por su postId
                 final check = widget.checksSeleccionados.firstWhere(
                   (c) => c.postId == value.toInt(),
                 );
+                  if (check == null) return Container();
 
-                // Si no se encuentra el `Check`, devolver un espacio vacío
-                if (check == null) return Container();
-
-                return Padding(
-                  padding: const EdgeInsets.only(top: 5),
-                  child: Column(
+                  return Column(
                     children: [
-                      Text(
-                        "Check" + check.postId.toString(),
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      Text(
-                        "Fecha:" + check.createdDate,
-                        style: TextStyle(fontSize: 15),
-                      ),
+                      Text("Check${check.postId}", style: TextStyle(fontSize: 16)),
+                      Text("Fecha: ${check.createdDate}", style: TextStyle(fontSize: 12)),
                     ],
-                  ),
-                );
-              },
+                  );
+                },
+              ),
             ),
           ),
         ),
       ),
-    );
-  }
+    ),
+  );
+}
+
 }
