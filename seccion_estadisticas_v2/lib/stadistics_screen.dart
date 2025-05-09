c48192679b8af8be09f7185512a33dbbd8535544
Andrescg01 <andrescg2001@gmail.com>
2025-05-09 12:19:48 +0200

    Opción de resaltar gráfico añadida

--- seccion_estadisticas_v2/lib/stadistics_screen.dart
+++ seccion_estadisticas_v2/lib/stadistics_screen.dart
@@ -9,17 +9,24 @@ class StadisticsScreen extends StatefulWidget {
 }
 
 class _StadisticsScreenState extends State<StadisticsScreen> {
+  int touchedIndexPieChart1 = -1;
+  int touchedIndexPieChart2 = -1;
+  int touchedIndexPieChart3 = -1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Estadísticas")),
       body: Column(
         children: [
           Row(
             children: [
               Expanded(
                 child: Padding(
-                  padding: const EdgeInsets.all(16.0),
+                  padding: const EdgeInsets.only(
+                    left: 12,
+                    top: 24,
+                    right: 24,
+                    bottom: 12,
+                  ),
                   child: SizedBox(
                     child: Column(
                       children: [
@@ -62,7 +69,12 @@ class _StadisticsScreenState extends State<StadisticsScreen> {
               ),
               Expanded(
                 child: Padding(
-                  padding: const EdgeInsets.all(8.0),
+                  padding: const EdgeInsets.only(
+                    left: 24,
+                    top: 24,
+                    right: 12,
+                    bottom: 12,
+                  ),
                   child: SizedBox(
                     height: 200,
                     child: LineChart(
@@ -139,7 +151,12 @@ class _StadisticsScreenState extends State<StadisticsScreen> {
               ),
               Expanded(
                 child: Padding(
-                  padding: const EdgeInsets.all(8.0),
+                  padding: const EdgeInsets.only(
+                    left: 24,
+                    top: 24,
+                    right: 12,
+                    bottom: 12,
+                  ),
                   child: SizedBox(
                     height: 400,
                     child: PieChart(
@@ -177,7 +194,12 @@ class _StadisticsScreenState extends State<StadisticsScreen> {
             children: [
               Expanded(
                 child: Padding(
-                  padding: const EdgeInsets.all(8.0),
+                  padding: const EdgeInsets.only(
+                    left: 12,
+                    top: 12,
+                    right: 24,
+                    bottom: 24,
+                  ),
                   child: SizedBox(
                     height: 200,
                     child: BarChart(
@@ -221,60 +243,272 @@ class _StadisticsScreenState extends State<StadisticsScreen> {
                 ),
               ),
               Expanded(
-                child: SizedBox(
-                  height: 200,
-                  child: PieChart(
-                    PieChartData(
-                      sections: [
-                        PieChartSectionData(
-                          value: 40,
-                          title: "40%",
-                          color: Colors.blue,
-                          radius: 100,
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    left: 12,
+                    top: 12,
+                    right: 12,
+                    bottom: 24,
+                  ),
+                  child: SizedBox(
+                    height: 200,
+                    child: PieChart(
+                      PieChartData(
+                        pieTouchData: PieTouchData(
+                          touchCallback: (
+                            FlTouchEvent event,
+                            PieTouchResponse? pieTouchResponse,
+                          ) {
+                            setState(() {
+                              if (!event.isInterestedForInteractions ||
+                                  pieTouchResponse == null ||
+                                  pieTouchResponse.touchedSection == null) {
+                                return;
+                              }
+                              touchedIndexPieChart2 =
+                                  pieTouchResponse
+                                      .touchedSection!
+                                      .touchedSectionIndex;
+                            });
+                          },
                         ),
-                        PieChartSectionData(
-                          value: 30,
-                          title: "30%",
-                          color: Colors.red,
-                          radius: 100,
-                        ),
-                        PieChartSectionData(
-                          value: 20,
-                          title: "20%",
-                          color: Colors.green,
-                          radius: 100,
-                        ),
-                      ],
+                        sections: [
+                          PieChartSectionData(
+                            value: 20,
+                            title: "20%",
+                            color: const Color(0xFF2196F3),
+                            radius: touchedIndexPieChart2 == 0 ? 120 : 100,
+                            titleStyle: TextStyle(
+                              fontSize: touchedIndexPieChart2 == 0 ? 16 : 12,
+                            ),
+                            borderSide: BorderSide(
+                              color:
+                                  touchedIndexPieChart2 == 0
+                                      ? Colors.black
+                                      : Color(0xFF2196F3),
+                              width: 2,
+                            ),
+                            badgeWidget: Container(
+                              width: touchedIndexPieChart2 == 0 ? 50 : 45,
+                              height: touchedIndexPieChart2 == 0 ? 50 : 45,
+                              decoration: BoxDecoration(
+                                //color: const Color.fromARGB(255, 12, 12, 12),
+                                shape: BoxShape.circle,
+                                image: DecorationImage(
+                                  image: AssetImage("icons/ios.png"),
+                                  fit: BoxFit.cover,
+                                ),
+                                border: Border.all(
+                                  width: touchedIndexPieChart2 == 0 ? 2 : 0,
+                                ),
+                              ),
+                            ),
+                            badgePositionPercentageOffset: 1.1,
+                          ),
+                          PieChartSectionData(
+                            value: 30,
+                            title: "30%",
+                            color: Colors.red,
+                            radius: touchedIndexPieChart2 == 1 ? 120 : 100,
+                            titleStyle: TextStyle(
+                              fontSize: touchedIndexPieChart2 == 1 ? 16 : 12,
+                            ),
+                            badgeWidget: Container(
+                              width: touchedIndexPieChart2 == 1 ? 50 : 45,
+                              height: touchedIndexPieChart2 == 1 ? 50 : 45,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                image: DecorationImage(
+                                  image: AssetImage("icons/android.png"),
+                                  fit: BoxFit.cover,
+                                ),
+                                border: Border.all(
+                                  width: touchedIndexPieChart2 == 1 ? 2 : 0,
+                                ),
+                              ),
+                            ),
+                            badgePositionPercentageOffset: 1.1,
+                          ),
+                          PieChartSectionData(
+                            value: 50,
+                            title: "50%",
+                            color: Colors.green,
+                            radius: touchedIndexPieChart2 == 2 ? 120 : 100,
+                            titleStyle: TextStyle(
+                              fontSize: touchedIndexPieChart2 == 2 ? 16 : 12,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
               Expanded(
-                child: SizedBox(
-                  height: 200,
-                  child: PieChart(
-                    PieChartData(
-                      sections: [
-                        PieChartSectionData(
-                          value: 40,
-                          title: "40%",
-                          color: Colors.blue,
-                          radius: 100,
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    left: 24,
+                    top: 12,
+                    right: 12,
+                    bottom: 24,
+                  ),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: SizedBox(
+                          width: 300,
+                          height: 200,
+                          child: PieChart(
+                            PieChartData(
+                              centerSpaceRadius: 0,
+                              pieTouchData: PieTouchData(
+                                touchCallback: (
+                                  FlTouchEvent event,
+                                  PieTouchResponse? pieTouchResponse,
+                                ) {
+                                  setState(() {
+                                    if (!event.isInterestedForInteractions ||
+                                        pieTouchResponse == null ||
+                                        pieTouchResponse.touchedSection ==
+                                            null) {
+                                      touchedIndexPieChart3 = -1;
+                                      return;
+                                    }
+                                    touchedIndexPieChart3 =
+                                        pieTouchResponse
+                                            .touchedSection!
+                                            .touchedSectionIndex;
+                                  });
+                                },
+                              ),
+                              sections: [
+                                PieChartSectionData(
+                                  value: 40,
+                                  title: "40%",
+                                  color: Colors.blue,
+                                  radius: touchedIndexPieChart3 == 0 ? 100 : 90,
+                                ),
+                                PieChartSectionData(
+                                  value: 30,
+                                  title: "30%",
+                                  color: Colors.red,
+                                  radius: touchedIndexPieChart3 == 1 ? 100 : 90,
+                                ),
+                                PieChartSectionData(
+                                  value: 20,
+                                  title: "20%",
+                                  color: Colors.green,
+                                  radius: touchedIndexPieChart3 == 2 ? 100 : 90,
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
-                        PieChartSectionData(
-                          value: 30,
-                          title: "30%",
-                          color: Colors.red,
-                          radius: 100,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: SizedBox(
+                          child: Column(
+                            children: [
+                              Row(
+                                children: [
+                                  Container(
+                                    width: touchedIndexPieChart3 == 0 ? 15 : 10,
+                                    height:
+                                        touchedIndexPieChart3 == 0 ? 15 : 10,
+                                    decoration: BoxDecoration(
+                                      color: Colors.blue,
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: SizedBox(
+                                      child: Text(
+                                        "Estado 1",
+                                        style: TextStyle(
+                                          fontSize:
+                                              touchedIndexPieChart3 == 0
+                                                  ? 25
+                                                  : 20,
+                                          fontWeight:
+                                              touchedIndexPieChart3 == 0
+                                                  ? FontWeight.bold
+                                                  : FontWeight.normal,
+                                        ),
+                                        overflow: TextOverflow.ellipsis,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Container(
+                                    width: touchedIndexPieChart3 == 1 ? 15 : 10,
+                                    height:
+                                        touchedIndexPieChart3 == 1 ? 15 : 10,
+                                    decoration: BoxDecoration(
+                                      color: Colors.red,
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: SizedBox(
+                                      child: Text(
+                                        "Estado 2",
+                                        style: TextStyle(
+                                          fontSize:
+                                              touchedIndexPieChart3 == 1
+                                                  ? 25
+                                                  : 20,
+                                          fontWeight:
+                                              touchedIndexPieChart3 == 1
+                                                  ? FontWeight.bold
+                                                  : FontWeight.normal,
+                                        ),
+                                        overflow: TextOverflow.ellipsis,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Container(
+                                    width: touchedIndexPieChart3 == 2 ? 15 : 10,
+                                    height:
+                                        touchedIndexPieChart3 == 2 ? 15 : 10,
+                                    decoration: BoxDecoration(
+                                      color: Colors.green,
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: SizedBox(
+                                      child: Text(
+                                        "Estado 3",
+                                        style: TextStyle(
+                                          fontSize:
+                                              touchedIndexPieChart3 == 2
+                                                  ? 25
+                                                  : 20,
+                                          fontWeight:
+                                              touchedIndexPieChart3 == 2
+                                                  ? FontWeight.bold
+                                                  : FontWeight.normal,
+                                        ),
+                                        overflow: TextOverflow.ellipsis,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
                         ),
-                        PieChartSectionData(
-                          value: 20,
-                          title: "20%",
-                          color: Colors.green,
-                          radius: 100,
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ),
               ),
