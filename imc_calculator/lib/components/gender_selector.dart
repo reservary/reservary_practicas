116446e6eb39b21684c57f7e5497604255296505
Miguel Fernandez <miguel.fernandez.villanueva@gregoriofer.com>
2025-05-09 13:06:07 +0200

    Aplicación IMC Calculator
    Aplicación IMC Calculator acabada

---
+++ imc_calculator/lib/components/gender_selector.dart
@@ -0,0 +1,84 @@
+import 'package:basic_flutter/core/app_colors.dart';
+import 'package:basic_flutter/core/text_styles.dart';
+import 'package:flutter/material.dart';
+
+class GenderSelector extends StatefulWidget {
+  const GenderSelector({super.key});
+
+  @override
+  State<GenderSelector> createState() => _GenderSelectorState();
+}
+
+class _GenderSelectorState extends State<GenderSelector> {
+String? selectedGender;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        //Hombre
+        Expanded(
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                selectedGender = "Hombre";
+              });
+            },
+            child: Padding(
+              padding: const EdgeInsets.only(left:16, top:16, bottom:16, right:8),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: selectedGender == "Hombre"
+                   ? AppColors.backgroundComponentSelected
+                   : AppColors.backgroundComponent,
+                  borderRadius: BorderRadius.circular(16)
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Column(
+                    children: [
+                      Image.asset("assets/images/male.png", height: 100,),
+                      SizedBox(height: 8,),
+                      Text("Hombre".toUpperCase(), style: TextStyles.bodyText,)
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+        //Mujer
+        Expanded(
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                selectedGender = "Mujer";
+              });
+            },
+            child: Padding(
+              padding: const EdgeInsets.only(left:8, top:16, bottom:16, right:16),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: selectedGender == "Mujer"
+                   ? AppColors.backgroundComponentSelected
+                   : AppColors.backgroundComponent,
+                  borderRadius: BorderRadius.circular(16)
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Column(
+                    children: [
+                      Image.asset("assets/images/female.png", height: 100,),
+                      SizedBox(height: 8,),
+                      Text("Mujer".toUpperCase(), style: TextStyles.bodyText,)
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
