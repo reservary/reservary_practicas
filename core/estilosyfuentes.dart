6248fa3c18462ab4452c89a8d0f1ade96e56339e
Luis Miguel Pacios Memis <luismiguel.pacios.memis@gregoriofer.com>
2025-05-15 09:04:52 +0200

    Delete core directory

--- core/estilosyfuentes.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'tema.dart'; // Import AppColors
-
-class TextStyles {
-  static const TextStyle bodyText = TextStyle(color: AppColors.accent, fontSize: 15);
-}
