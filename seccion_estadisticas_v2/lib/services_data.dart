e28b873973130426e144208172fc616451ed7c85
Andrescg01 <andrescg2001@gmail.com>
2025-05-16 13:25:37 +0200

    Filtro de empleados y servicios implementado

--- seccion_estadisticas_v2/lib/services_data.dart
+++ seccion_estadisticas_v2/lib/services_data.dart
@@ -12,63 +12,199 @@ class ServicesData extends StatefulWidget {
 }
 
 class _ServicesDataState extends State<ServicesData> {
+  List<String> selectedServices = [];
+
+  @override
+  void initState() {
+    super.initState();
+    selectedServices = [];
+  }
+
+  void _showServicesSelector() async {
+    final List<String> servicesNames =
+        widget.stats.totalBookingsPerService.keys.toList();
+    final tempSelected = List<String>.from(selectedServices);
+
+    final result = await showDialog<List<String>>(
+      context: context,
+      builder: (context) {
+        return StatefulBuilder(
+          builder: (context, setStateDialog) {
+            return AlertDialog(
+              title: Text("Seleccione los servicios"),
+              content: SingleChildScrollView(
+                child: Column(
+                  children:
+                      servicesNames.map((serv) {
+                        return CheckboxListTile(
+                          title: Text(serv),
+                          value: tempSelected.contains(serv),
+                          onChanged: (bool? checked) {
+                            setStateDialog(() {
+                              if (checked == true) {
+                                tempSelected.add(serv);
+                              } else {
+                                tempSelected.remove(serv);
+                              }
+                            });
+                          },
+                        );
+                      }).toList(),
+                ),
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: Text("Cancelar"),
+                ),
+                ElevatedButton(
+                  onPressed:
+                      tempSelected.isEmpty
+                          ? null
+                          : () {
+                            Navigator.pop(context, tempSelected);
+                          },
+                  child: Text("Aplicar"),
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+    if (result != null) {
+      setState(() {
+        selectedServices = result;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    final servicesNames = widget.stats.totalBookingsPerService.keys.toList();
-    final servicesValues = widget.stats.totalBookingsPerService.values.toList();
+    final dataServices = widget.stats.totalBookingsPerService;
+    final filteredNames =
+        dataServices.entries
+            .where((entry) => selectedServices.contains(entry.key))
+            .map((entry) => entry.key)
+            .toList();
+    final filteredValues =
+        dataServices.entries
+            .where((entry) => selectedServices.contains(entry.key))
+            .map((entry) => entry.value)
+            .toList();
+
+    if (filteredNames.isEmpty) {
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: _showServicesSelector,
+              child: Text("Seleccionar servicios"),
+            ),
+            SizedBox(height: 16),
+            Text(
+              "No hay ningún servicio seleccionado",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      );
+    }
+
     return SizedBox(
       height: 315,
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: BarChart(
-          BarChartData(
-            rotationQuarterTurns: 45,
-            gridData: FlGridData(show: false),
-            borderData: FlBorderData(
-              show: true,
-              border: Border(
-                top: BorderSide.none,
-                right: BorderSide.none,
-                left: BorderSide(width: 1),
-                bottom: BorderSide(width: 1),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    onPressed: _showServicesSelector,
+                    child: Text("Seleccionar servicios"),
+                  ),
+                ),
               ),
-            ),
-            titlesData: FlTitlesData(
-              rightTitles: AxisTitles(
-                sideTitles: SideTitles(showTitles: false),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        selectedServices.clear();
+                      });
+                    },
+                    child: Text("Restablecer"),
+                  ),
+                ),
               ),
-              topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
-              bottomTitles: AxisTitles(
-                sideTitles: SideTitles(
-                  reservedSize: 50,
-                  showTitles: true,
-                  getTitlesWidget: (value, meta) {
-                    String services = servicesNames[value.toInt()];
-                    return Transform.rotate(
-                      angle: -math.pi / 2,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(services, style: TextStyle(fontSize: 10)),
+            ],
+          ),
+          Expanded(
+            child: SizedBox(
+              child: BarChart(
+                BarChartData(
+                  rotationQuarterTurns: 45,
+                  gridData: FlGridData(show: false),
+                  borderData: FlBorderData(
+                    show: true,
+                    border: Border(
+                      top: BorderSide.none,
+                      right: BorderSide.none,
+                      left: BorderSide(width: 1),
+                      bottom: BorderSide(width: 1),
+                    ),
+                  ),
+                  titlesData: FlTitlesData(
+                    rightTitles: AxisTitles(
+                      sideTitles: SideTitles(showTitles: false),
+                    ),
+                    topTitles: AxisTitles(
+                      sideTitles: SideTitles(showTitles: false),
+                    ),
+                    bottomTitles: AxisTitles(
+                      sideTitles: SideTitles(
+                        reservedSize: 50,
+                        showTitles: true,
+                        getTitlesWidget: (value, meta) {
+                          if (value.toInt() < 0 ||
+                              value.toInt() >= filteredNames.length) {
+                            return SizedBox.shrink();
+                          }
+                          final serviceName = filteredNames[value.toInt()];
+                          return Transform.rotate(
+                            angle: -math.pi / 2,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                serviceName,
+                                style: TextStyle(fontSize: 10),
+                              ),
+                            ),
+                          );
+                        },
                       ),
+                    ),
+                  ),
+                  barGroups: List.generate(filteredValues.length, (index) {
+                    return BarChartGroupData(
+                      x: index,
+                      barRods: [
+                        BarChartRodData(
+                          toY: filteredValues[index].toDouble(),
+                          color: Colors.blue,
+                          width: 10,
+                        ),
+                      ],
                     );
-                  },
+                  }),
                 ),
               ),
             ),
-            barGroups: List.generate(servicesNames.length, (index) {
-              return BarChartGroupData(
-                x: index,
-                barRods: [
-                  BarChartRodData(
-                    toY: servicesValues[index].toDouble(),
-                    color: Colors.blue,
-                    width: 10,
-                  ),
-                ],
-              );
-            }),
           ),
-        ),
+        ],
       ),
     );
   }
